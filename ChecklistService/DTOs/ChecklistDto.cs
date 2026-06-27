e7ce96d5af31d7cd8abbd0219131452e78ea5697
rosandazivanovic <rosa.zivanovic03@gmail.com>
2026-06-27 14:39:54 +0200

    Add Checklist DTOs

---
+++ ChecklistService/DTOs/ChecklistDto.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ChecklistService.DTOs
+{
+    public class CreateChecklistItemDto
+    {
+        [Required]
+        [StringLength(200, MinimumLength = 1)]
+        public string Name { get; set; } = string.Empty;
+    }
+
+    public class ChecklistItemResponseDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public bool IsCompleted { get; set; }
+    }
+}
