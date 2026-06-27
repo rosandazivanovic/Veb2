da6646cc2d61e92aa46eda88f6f3b72b8bd4d81b
rosandazivanovic <rosa.zivanovic03@gmail.com>
2026-06-27 14:45:56 +0200

    Add SharedPlanCacheEntry model for Reliable Dictionary

---
+++ TravelService/Models/SharedPlanCacheEntry.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace TravelService.Models
+{
+    [DataContract]
+    public class SharedPlanCacheEntry
+    {
+        [DataMember]
+        public int TravelPlanId { get; set; }
+
+        [DataMember]
+        public string AccessType { get; set; } = string.Empty;
+
+        [DataMember]
+        public DateTime ExpiresAt { get; set; }
+    }
+}
