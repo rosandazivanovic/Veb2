b5619357fd2472307ff14638d77575ee203a9a35
rosandazivanovic <rosa.zivanovic03@gmail.com>
2026-06-27 14:36:40 +0200

    Add ChecklistItem domain model

---
+++ ChecklistService/Models/ChecklistItem.cs
@@ -0,0 +1,10 @@
+﻿namespace ChecklistService.Models
+{
+    public class ChecklistItem
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public bool IsCompleted { get; set; } = false;
+        public int TravelPlanId { get; set; }
+    }
+}
