27d6219e3cc4ce08c1e68b4f315d803445362590
rosandazivanovic <rosa.zivanovic03@gmail.com>
2026-06-27 14:31:53 +0200

    Add Travel database context

---
+++ TravelService/Data/AppDbContext.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TravelService.Models;
+
+namespace TravelService.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<TravelPlan> TravelPlans { get; set; } = null!;
+        public DbSet<Destination> Destinations { get; set; } = null!;
+        public DbSet<Activity> Activities { get; set; } = null!;
+        public DbSet<SharedPlan> SharedPlans { get; set; } = null!;
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<TravelPlan>()
+                .HasMany(p => p.Destinations)
+                .WithOne(d => d.TravelPlan)
+                .HasForeignKey(d => d.TravelPlanId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            modelBuilder.Entity<TravelPlan>()
+                .HasMany(p => p.Activities)
+                .WithOne(a => a.TravelPlan)
+                .HasForeignKey(a => a.TravelPlanId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            modelBuilder.Entity<TravelPlan>()
+                .HasMany(p => p.SharedPlans)
+                .WithOne(s => s.TravelPlan)
+                .HasForeignKey(s => s.TravelPlanId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            modelBuilder.Entity<TravelPlan>()
+                .Property(p => p.Budget)
+                .HasPrecision(18, 2);
+
+            modelBuilder.Entity<Activity>()
+                .Property(a => a.EstimatedCost)
+                .HasPrecision(18, 2);
+
+            modelBuilder.Entity<SharedPlan>()
+                .Property(s => s.Token)
+                .IsRequired();
+
+            modelBuilder.Entity<SharedPlan>()
+                .Property(s => s.AccessType)
+                .IsRequired();
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ TravelService/Data/AppDbContextFactory.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace TravelService.Data
+{
+    public class AppDbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
+    {
+        public AppDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
+            optionsBuilder.UseSqlServer("Server=DESKTOP-6FSS16F\\SQLEXPRESS;Database=TravelPlannerDB;Trusted_Connection=True;TrustServerCertificate=True;");
+
+            return new AppDbContext(optionsBuilder.Options);
+        }
+    }
+}
