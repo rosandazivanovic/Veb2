bf14406b88da2b2d7ee9436060366c26a59ea1c1
rosandazivanovic <rosa.zivanovic03@gmail.com>
2026-06-27 14:43:17 +0200

    Add Expense domain model

---
+++ ExpenseService/Models/Expense.cs
@@ -0,0 +1,16 @@
+﻿namespace ExpenseService.Models
+{
+    public class Expense
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+
+        public string Category { get; set; } = string.Empty;
+
+        public decimal Amount { get; set; }
+        public DateTime Date { get; set; }
+        public string Description { get; set; } = string.Empty;
+
+        public int TravelPlanId { get; set; }
+    }
+}
