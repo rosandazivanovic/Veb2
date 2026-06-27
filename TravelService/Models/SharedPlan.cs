9f1bdce05f4871b0349b469d3fd8bc707f31bde4
rosandazivanovic <rosa.zivanovic03@gmail.com>
2026-06-27 14:45:46 +0200

    Add SharedPlan model

---
+++ TravelService/Models/SharedPlan.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace TravelService.Models
+{
+    public class SharedPlan
+    {
+        public int Id { get; set; }
+
+        public string Token { get; set; } = Guid.NewGuid().ToString();
+
+        public string AccessType { get; set; } = "VIEW";
+
+        public DateTime ExpiresAt { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        public int TravelPlanId { get; set; }
+        public TravelPlan TravelPlan { get; set; } = null!;
+    }
+}
