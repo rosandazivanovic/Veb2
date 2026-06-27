41e11cca4412d9860d4dfaebb76071a2378c89fb
rosandazivanovic <rosa.zivanovic03@gmail.com>
2026-06-27 14:43:32 +0200

    Add Expense database context

---
+++ ExpenseService/Data/AppDbContext .cs
@@ -0,0 +1,31 @@
+﻿using ExpenseService.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection.Emit;
+
+namespace ExpenseService.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Expense> Expenses { get; set; } = null!;
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Expense>()
+                .Property(e => e.Amount)
+                .HasPrecision(18, 2);
+
+            modelBuilder.Entity<Expense>()
+                .Property(e => e.Name)
+                .IsRequired()
+                .HasMaxLength(150);
+
+            modelBuilder.Entity<Expense>()
+                .Property(e => e.Category)
+                .IsRequired();
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ ExpenseService/Data/AppDbContextFactory .cs
@@ -0,0 +1,18 @@
+﻿using ExpenseService.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace ExpenseService.Data
+{
+    public class AppDbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
+    {
+        public AppDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
+            optionsBuilder.UseSqlServer(
+                "Server=DESKTOP-6FSS16F\\SQLEXPRESS;Database=ExpenseDB;Trusted_Connection=True;TrustServerCertificate=True;");
+
+            return new AppDbContext(optionsBuilder.Options);
+        }
+    }
+}
