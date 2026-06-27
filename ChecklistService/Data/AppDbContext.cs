1d0a286e4fcd5a5d9a6604aa43207288ba19f927
rosandazivanovic <rosa.zivanovic03@gmail.com>
2026-06-27 14:36:54 +0200

    Add Checklist database context

---
+++ ChecklistService/Data/AppDbContext.cs
@@ -0,0 +1,23 @@
+﻿using ChecklistService.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection.Emit;
+
+namespace ChecklistService.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<ChecklistItem> ChecklistItems { get; set; } = null!;
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<ChecklistItem>()
+                .Property(c => c.Name)
+                .IsRequired()
+                .HasMaxLength(200);
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ ChecklistService/Data/AppDbContextFactory .cs
@@ -0,0 +1,19 @@
+﻿using ChecklistService.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+
+namespace ChecklistService.Data
+{
+    public class AppDbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
+    {
+        public AppDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
+            optionsBuilder.UseSqlServer(
+                "Server=DESKTOP-6FSS16F\\SQLEXPRESS;Database=ChecklistDB;Trusted_Connection=True;TrustServerCertificate=True;");
+
+            return new AppDbContext(optionsBuilder.Options);
+        }
+    }
+}
